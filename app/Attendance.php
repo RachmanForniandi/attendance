281db694082b1ed1d9d939e8e5c771b8ee1e00a5
RachmanForniandi <rachmanforniandi@gmail.com>
2020-11-27 02:34:27 +0700

    instalasi laravel & create table database done

---
+++ app/Attendance.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    protected $guarded =[];
+
+    public function detail(){
+        return $this->hasMany(AttendanceDetail::class);
+    }
+}
---
+++ app/AttendanceDetail.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AttendanceDetail extends Model
+{
+    protected $guarded =[];
+
+    public function detail(){
+        return $this->hasMany(Attendance::class);
+    }
+}
